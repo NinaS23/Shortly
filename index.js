85b4da8f26a1b50d139ae5746261b88a9257e9e0
MariaDuda <mariabevenuto062@gmail.com>
2022-08-02 12:06:12 -0300

    feat port runing well

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//oi
+import express, {json} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const server = express();
+server.use(json());
+server.use(cors());
+
+const PORT= process.env.PORT || 5000
+server.listen(PORT ,()=>{
+    console.log(`subiu a porta ${PORT} 
+    `)
+})  
