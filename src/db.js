e835cf325ba96f3a9b7a758b0c59c07693a126e3
MariaDuda <mariabevenuto062@gmail.com>
2022-08-06 13:23:54 -0300

    ready for deploy

--- src/db.js
+++ src/db.js
@@ -1,23 +1,22 @@
-import pg from 'pg';
-import dotenv from "dotenv";
 
-dotenv.config()
+import dotenv from "dotenv";
+import pg from 'pg';
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = process.env.SENHA_DB;
-const host = 'localhost';
-const port = 5432;
-const database = process.env.DATABASE;
 
-const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-});
+dotenv.config()
+
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
+
+const connection = new Pool(databaseConfig);
+
+export default connection;
 
 export {
     connection
